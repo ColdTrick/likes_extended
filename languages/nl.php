d4e3b16a441007e0a0634a120f59aa497804009d
Jerôme Bakker <jeabakker@coldtrick.com>
2026-03-10 15:25:22 +0100

    chore: added Dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This file was created by Translation Editor v14.0.4
+ * On 2026-03-10 15:24
+ */
+
+return array (
+  'likes_extended:likes:tab' => 'Likes',
+  'likes_extended:num_likes:text:single' => '%s like',
+  'likes_extended:num_likes:text:plural' => '%s likes',
+  'likes_extended:num_likes:title' => 'Bekijk likes',
+);
