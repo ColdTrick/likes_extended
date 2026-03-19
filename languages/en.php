fb81d351459aacc0a3ce92e69c30eea37dcc1c60
Jerôme Bakker <jeabakker@coldtrick.com>
2026-03-19 09:02:04 +0100

    chore: missing translation

--- languages/en.php
+++ languages/en.php
@@ -3,6 +3,9 @@
 return [
 	'likes_extended:likes:tab' => "Likes",
 	
+	'likes_extended:menu:social:likes_dropdown:text' => "Choose",
+	'likes_extended:menu:social:likes_dropdown:title' => "Choose your like",
+	
 	'likes_extended:num_likes:text:single' => "%s like",
 	'likes_extended:num_likes:text:plural' => "%s likes",
 	'likes_extended:num_likes:title' => "See likes",
--- languages/nl.php
+++ languages/nl.php
@@ -1,10 +1,12 @@
 <?php
 /**
  * This file was created by Translation Editor v14.0.4
- * On 2026-03-10 15:24
+ * On 2026-03-19 09:01
  */
 
 return array (
+  'likes_extended:menu:social:likes_dropdown:text' => 'Kies',
+  'likes_extended:menu:social:likes_dropdown:title' => 'Kies je like',
   'likes_extended:likes:tab' => 'Likes',
   'likes_extended:num_likes:text:single' => '%s like',
   'likes_extended:num_likes:text:plural' => '%s likes',
